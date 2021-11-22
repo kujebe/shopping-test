ea8c769392895a9198fea301a218b9fb18d79152
kujebe <kujebeseun@gmsil.com>
2021-11-22 12:34:40 +0100

    Initial configuration including router-extra

---
+++ pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h1 class="home">{{ title }}</h1>
+    <p class="home__sub-heading">A good shopping mall!</p>
+  </section>
+</template>
+
+<router>
+  {
+    name: 'Shop',
+    path: '/'
+  }
+</router>
+
+<script src="./Home.ts" lang="ts"/>
+<style src="./Home.scss" lang="scss" scoped/>
